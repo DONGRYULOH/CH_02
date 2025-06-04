7999559d2f10f2c736de609b823ded770efacf37
unknown <dhehdfbf4587@naver.com>
2025-06-04 11:56:53 +0900

    test

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,20 +1,45 @@
-﻿// ConsoleApplication1.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
+﻿#include <iostream>
+using namespace std;
 
-#include <iostream>
+class Animal {
+public:
+    virtual ~Animal() {}
+
+public:
+    virtual void makeSound() = 0;
+};
+
+class Dog : public Animal{
+public:
+    void makeSound() {
+        cout << "Dog sound";
+    }
+};
+
+class Cat : public Animal {
+public:
+    void makeSound() {
+        cout << "Cat sound";
+    }
+};
+
+class Cow : public Animal {
+public:
+    void makeSound() {
+        cout << "Cow sound";
+    }
+};
 
 int main()
-{
-    std::cout << "Hello World!\n";ㅇㅇ
+{    
+    Animal* animal[3];
+    animal[0] = new Dog();
+    animal[1] = new Cat();
+    animal[2] = new Cow();
+
+    for (int i = 0; i < 3; i++) {
+        (*animal[i]).makeSound();
+    }
 }
 
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
 
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
